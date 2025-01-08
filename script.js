4f47e2d46b5f9e3e746fb28f821d6f5d1c4efc74
Ian <spinto774@yahoo.com>
2025-01-08 17:40:22 +0800

    Add delay for each shuffle iteration so you can actually see the shuffling, avoid shuffling to same activity consecutively

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ class Activity {
 
 // How many times to shuffle when button clicked
 const shuffleCount = 4;
+// Delay between each shuffle iteration in ms
+const shuffleDelay = 200;
 
 const activities = [
     new Activity(
@@ -28,6 +30,9 @@ const activities = [
     )
 ];
 
+// Index of current displayed activity, or -1 if no activity displayed yet (button not pressed)
+let currentActivity = -1;
+
 // Only try to access DOM elements once loading complete (avoid null exceptions)
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -36,18 +41,50 @@ document.addEventListener("DOMContentLoaded", function () {
     const desc = document.getElementById("activity-desc");
     const section = document.getElementById("main-section");
 
-    // todo implement short wait in between
-    function shuffle() {
-        // todo refactor so the same element won't be shuffled to twice
-        for (let i = 0; i < 4; i++) {
-            const activity = activities[Math.floor(Math.random() * activities.length)];
-            header.innerHTML = activity.header;
-            desc.innerHTML = activity.desc;
-            section.style.backgroundImage = `url('./media/activities/${activity.imgPath}')`;
+    /**
+     * Shuffle once. Return index of activity chosen. lastActivity is the idnex of lastactivity displayed (choose a different activity)
+     */
+    function shuffleOnce(lastActivity) {
+
+        // Pick random activity different to current one
+        let activityIndex = Math.floor(Math.random() * activities.length);
+        if (activityIndex == lastActivity) {
+            if (activityIndex == 0) {
+                activityIndex++;
+            } else {
+                activityIndex--;
+            }
         }
+        const activity = activities[activityIndex];
+
+        // Style HTML elements
+        header.innerHTML = activity.header;
+        desc.innerHTML = activity.desc;
+        section.style.backgroundImage = `url('./media/activities/${activity.imgPath}')`;
         header.classList.remove("hidden");
+
+        return activityIndex;
     }
 
-    shuffleBtn.addEventListener("click", shuffle);
+    /**
+     * Shuffle a given number of times
+     */
+    function shuffleMultipleTimes(iterations, lastActivity) {
+        shuffleBtn.disabled = true;
+        const activityChosen = shuffleOnce(lastActivity);
+        if (iterations > 0) {
+            setTimeout(() => shuffleMultipleTimes(iterations - 1, activityChosen), shuffleDelay);
+        } else {
+            // Last shuffle complete
+            currentActivity = activityChosen;
+            shuffleBtn.disabled = false;
+        }
+    }
+
+    function shuffleBtnClicked() {
+        shuffleMultipleTimes(shuffleCount, currentActivity);
+    }
+
+    shuffleBtn.addEventListener("click", shuffleBtnClicked);
     
 });
